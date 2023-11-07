6f31825a88cf54c5610948541023ce2dcfeda6bd
BrandonDoesEmails <blester1989@gmail.com>
2023-11-06 21:33:30 -0500

    get game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+
+
+const useTrailers = (gameId: number) => {
+   const trailers = new APIClient<Trailer>(`/games/${gameId}/movies`)
+
+   return useQuery({
+        queryKey: ['trailers', gameId],
+        queryFn: trailers.getAll
+    })
+}
+
+export default useTrailers;
